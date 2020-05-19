92d765cafe97553454b594db7c68f3ee30c21dfe
ankitaredhat <50073227+ankitaredhat@users.noreply.github.com>
2020-05-19 07:34:07 -0700

    string-str-function.py

---
+++ string-str-function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#working with methods/functions in python relating to method Str()...
+#string-str-function.py
+
+""" 
+the str() function converts the non-string values into string values
+str(2) will resture "2"
+"""
+
+"""Declare and assign your variable on line 4,
+then call your method on line 5!"""
+pi = 3.14
+pi_str = str(pi)
+print "converted into text value of pi", pi_str
+print  str(pi)
+
+print "\n This computer program was developed  by Tom"
