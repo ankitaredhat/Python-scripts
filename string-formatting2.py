a9e9f8a97f09950a56ac77bad58abad4c121294d
ankitaredhat <50073227+ankitaredhat@users.noreply.github.com>
2020-05-19 07:37:02 -0700

    string-formatting2.py

---
+++ string-formatting2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#Using the String Formating2 in python language... 
+# NOT using the default concatenating... use % to replace the strings and concat 'em
+string_1 = "ABCDEF"
+string_2 = "XYZ"
+print "Greetings %s, %s is a Farzi company" % ( string_1, string_2 )
