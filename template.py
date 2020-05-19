6ab434f0421e483dea1c0a79dd5b353cc17935b3
ankitaredhat <50073227+ankitaredhat@users.noreply.github.com>
2020-05-19 07:33:03 -0700

    template.py

---
+++ template.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#Using the mathematical operations in python language... 
+print """This will demonstrate the usage of mathematical expressions
+in pyhton language"""
+sum = 1 + 2
+product = 1 * 3
+difference = 3 - 1
+division = 6 / 2
+exponent = 3 ** 9
+modulus = 4 % 2
+print "Sum of 1 and 2 = " sum
+print "\n"
