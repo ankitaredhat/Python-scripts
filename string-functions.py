edcea0d7ea55cc942ff35a81c48803b18affe4f9
ankitaredhat <50073227+ankitaredhat@users.noreply.github.com>
2020-05-19 07:35:55 -0700

    string-functions.py

---
+++ string-functions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#working with methods/functions in python relating to strings...
+#string-functions.py
+# This will change the upper case letter to lower case
+"""
+Parrot is a variable that stores the string below
+the len function helps to calculate the lenght of the string of the variable
+"""
+parrot = "Norwegian Blue"
+print "length of variable parrot =",  len(parrot)
+print "\n Printing the string ABC in lower case"
+print "ABC".lower()
+print "\n Printing the value of variable parrot in lower case"
+print parrot.lower()
+print "\n Printing \"banner\" in upper case"
+print "banner".upper()
+print "\n This computer program was developed  by ABC"
