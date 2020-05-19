8f4942492786b995feed65e2e456d4c357e73f11
ankitaredhat <50073227+ankitaredhat@users.noreply.github.com>
2020-05-19 07:31:16 -0700

    Create variable-type.py

---
+++ variable-type.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python  #<<<----- This is the path to the python binary executable
+# This is comment!
+# Title Working with variables...
+my_int = 10 		#INTEGER
+my_float = 1.23		#FLOAT
+my_bool = True		#BOOLEAN - No quotes
+print "Value of my_variables"
+print my_int, my_float, my_bool
