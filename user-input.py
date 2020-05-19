11e8651062ca060c25760a4c195524bd70fefde8
ankitaredhat <50073227+ankitaredhat@users.noreply.github.com>
2020-05-19 07:31:49 -0700

    Create user-input.py

---
+++ user-input.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#Using the User Input in python language... 
+
+name = raw_input("What is your name?")
+job = raw_input("What is your job?")
+expertise = raw_input("What is your expertise?")
+
+print "Ah, so your name is %s, your job is %s, " \
+"and your expertise is %s." % (name, job, expertise)
