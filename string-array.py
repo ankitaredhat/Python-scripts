f661165bb8fc8817324e971bd94b168031f612a7
ankitaredhat <50073227+ankitaredhat@users.noreply.github.com>
2020-05-20 17:25:23 -0700

    string-array.py

---
+++ string-array.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#working with the strings using the index
+#special-index-array.py 
+"""
+The string "PYTHON" has six characters,
+numbered 0 to 5, as shown below:
+
++---+---+---+---+---+---+
+| P | Y | T | H | O | N |
++---+---+---+---+---+---+
+  0   1   2   3   4   5
+
+So if you wanted "Y", you could just type
+"PYTHON"[1] (always start counting from 0!)
+"""
+fifth_letter = "RussIA"[5]
+
+print fifth_letter
+print "RussIA"[4]  + ' is the fourth letter in input string "Russia"'
+#print \'"RussIA"[4]\'  + ' is the fourth letter in input string "Russia"'
