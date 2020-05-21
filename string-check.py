52c1ee182f3b4c2dc098e310b04df0ebd13d46ba
ankitaredhat <50073227+ankitaredhat@users.noreply.github.com>
2020-05-20 17:19:05 -0700

    string-check.py

---
+++ string-check.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#to print the concatenated strings to the standaard output
+#string-check.py
+string1 = "Tom"
+string2 = "Cruise"
+print string1 + string2 + " has concatenated the two strings..."
