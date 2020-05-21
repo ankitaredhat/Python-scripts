6f0b2aab3f912661502c209431f6e290a5c538f8
ankitaredhat <50073227+ankitaredhat@users.noreply.github.com>
2020-05-20 17:26:33 -0700

    special-char.py

---
+++ special-char.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#working with the strings
+#special-char.py 
+string1 = "Country"
+string2 = "India"
+print string1 + string2 + " has concatenated the two strings..."
+#print this isn't gonna print.
+print 
+print 'this isn\'t gonna print without the reverse slash and the quotes'
