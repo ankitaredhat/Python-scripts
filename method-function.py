7833bf9021ae2c694bbf21d7fb7e9bc16f0d0133
ankitaredhat <50073227+ankitaredhat@users.noreply.github.com>
2020-05-20 17:28:28 -0700

    method-function.py

---
+++ method-function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#clearing the terminal
+#/usr/bin/clear
+def spam():
+# ':' at the end of the funtion is essential to declare a funtion
+	eggs = 12
+	return eggs
+#THE Variable definitions need to be indented right else it won't work!!
+print spam()
+print "\nYou may review the code difference in :"
+print "'method-function-error.py' and 'method-function.py'"
+#print "Difference between the erroroneous and correct variable declarations"
+#eval `diff method-function-error.py method-function.py'
+print "\n"
